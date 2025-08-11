c2a7ffe3533f24c961205f561c2640b75d280066
Pavel Deakin <kinvel.1490@yandex.ru>
2025-08-11 17:49:10 +0500

    try_1

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,45 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState } from 'react';
+import { clsx } from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormPropsType = {
+	children: React.ReactNode | React.ReactNode[] | null;
+	actions?: {
+		onSubmit?: (e: React.MouseEvent) => void;
+		onReset?: () => void;
+	};
+};
+
+export const ArticleParamsForm = (props: ArticleParamsFormPropsType) => {
+	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen(!isOpen);
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					{props.children}
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={props.actions?.onReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='button'
+							type='apply'
+							onClick={props.actions?.onSubmit}
+						/>
 					</div>
 				</form>
 			</aside>
---
+++ src/ui/spacer/spaser.tsx
@@ -0,0 +1,5 @@
+import styles from './spacer.module.scss';
+
+export const Spacer = () => {
+	return <div className={styles.spacer}></div>;
+};
