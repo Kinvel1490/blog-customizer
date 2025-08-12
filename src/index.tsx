6144fbde1ad729ceb8003b2031105804a360165d
Pavel Deakin <kinvel.1490@yandex.ru>
2025-08-12 11:09:22 +0500

    try_2

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { StrictMode, CSSProperties, useState, SyntheticEvent } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
 import {
 	defaultArticleState,
-	OptionType,
-	fontFamilyOptions,
 	ArticleStateType,
-	fontSizeOptions,
-	fontColors,
-	backgroundColors,
-	contentWidthArr,
 } from './constants/articleProps';
-import { Select } from './ui/select';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
-import { Separator } from './ui/separator';
-import { Spacer } from './ui/spacer';
-import { Text } from './ui/text';
-import { RadioGroup } from './ui/radio-group';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
@@ -30,7 +19,7 @@ const App = () => {
 	const [appState, setAppState] = useState(defaultArticleState);
 	const [formState, setFormState] = useState(defaultArticleState);
 
-	function handleOnSubmit(e: React.MouseEvent) {
+	function handleOnSubmit(e: SyntheticEvent) {
 		e.preventDefault();
 		setAppState({
 			...formState,
@@ -63,69 +52,12 @@ const App = () => {
 			}>
 			<ArticleParamsForm
 				actions={{
+					onChange: setParameters,
 					onSubmit: handleOnSubmit,
 					onReset: handleReset,
-				}}>
-				<Text weight={800} size={31} uppercase>
-					Задайте параметры
-				</Text>
-				<Spacer />
-				<Text weight={800} size={12} uppercase>
-					Шрифт
-				</Text>
-				<Select
-					options={fontFamilyOptions}
-					selected={formState.fontFamilyOption}
-					onChange={(fontOption: OptionType) => {
-						setParameters({ fontFamilyOption: fontOption });
-					}}
-				/>
-				<Spacer />
-				<RadioGroup
-					title='Размер шрифта'
-					options={fontSizeOptions}
-					selected={formState.fontSizeOption}
-					name='fontSizeOptions'
-					onChange={(fontSizeOption: OptionType) => {
-						setParameters({ fontSizeOption: fontSizeOption });
-					}}
-				/>
-				<Spacer />
-				<Text weight={800} size={12} uppercase>
-					Цвет шрифта
-				</Text>
-				<Select
-					options={fontColors}
-					selected={formState.fontColor}
-					onChange={(fontColor: OptionType) => {
-						setParameters({ fontColor: fontColor });
-					}}
-				/>
-				<Spacer />
-				<Separator />
-				<Spacer />
-				<Text weight={800} size={12} uppercase>
-					Цвет фона
-				</Text>
-				<Select
-					options={backgroundColors}
-					selected={formState.backgroundColor}
-					onChange={(backgroundColor: OptionType) => {
-						setParameters({ backgroundColor: backgroundColor });
-					}}
-				/>
-				<Spacer />
-				<Text weight={800} size={12} uppercase>
-					Ширина контента
-				</Text>
-				<Select
-					options={contentWidthArr}
-					selected={formState.contentWidth}
-					onChange={(contentWidth: OptionType) => {
-						setParameters({ contentWidth: contentWidth });
-					}}
-				/>
-			</ArticleParamsForm>
+				}}
+				articleState={formState}
+			/>
 			<Article />
 		</main>
 	);
--- src/ui/button/Button.tsx
+++ src/ui/button/Button.tsx
@@ -10,7 +10,7 @@ export const Button = ({
 	type,
 }: {
 	title: string;
-	onClick?: (e: React.MouseEvent) => void;
+	onClick?: () => void;
 	htmlType?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
 	type: 'apply' | 'clear';
 }) => {
